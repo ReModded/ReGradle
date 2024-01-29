c303e9032becd9e053f6e28bbf8b12ad6a9d502f
Twarug <patryk280502@gmail.com>
2024-01-29 22:55:08 +0100

    Add plugin publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     `kotlin-dsl`
-    `maven-publish`
+    id("com.gradle.plugin-publish") version "1.2.1"
 }
 
 group = "dev.remodded"
-version = "1.0.0"
+version = "1.0.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -18,10 +18,30 @@ kotlin {
 }
 
 gradlePlugin {
+    website = "https://github.com/ReModded/ReGradle"
+    vcsUrl = "https://github.com/ReModded/ReGradle"
     plugins {
-        create("dev.remodded.regradle") {
+        create("ReGradle") {
             id = "dev.remodded.regradle"
+            displayName = name
+            description = "ReGradle is a Gradle plugin for ReModded projects."
             implementationClass = "dev.remodded.regradle.ReGradlePlugin"
+            tags = listOf("ReModded", "Kotlin", "Minecraft Plugins")
+        }
+    }
+}
+
+publishing {
+    repositories {
+        val username: String? by project
+        val password: String? by project
+        maven {
+            name = "ReModded"
+            url = uri("https://repo.remodded.dev/repository/maven-snapshots/")
+            credentials {
+                this.username = username
+                this.password = password
+            }
         }
     }
 }
