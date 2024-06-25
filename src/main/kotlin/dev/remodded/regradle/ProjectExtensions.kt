d35c68fa7a6141257c401b2565d024647d9e17b2
Twarug <patryk280502@gmail.com>
2024-06-25 22:37:02 +0200

    Add more generated project properties

--- src/main/kotlin/dev/remodded/regradle/ProjectExtensions.kt
+++ src/main/kotlin/dev/remodded/regradle/ProjectExtensions.kt
@@ -11,14 +11,17 @@ fun Project.getPluginProps(): Map<String, String> {
         .map { prop -> prop.key.drop(1) to prop.value as String }
         .toMap(mutableMapOf())
 
-    val name = props["name"] ?: "ReTemplate"
+    val name = props.getOrPut("name") { "ReTemplate" }
     val platform = this.name.uppercaseFirstChar()
-    val group = props["group"] ?: "dev.remodded"
+    val group = props.getOrPut("group") { "dev.remodded" }
+    val rootPackage = "$group.${name.lowercase()}.${platform.lowercase().replace('-', '_')}"
 
     val mainClassname =
         "$name${if (platform.contains('-')) platform.substring(0, platform.lastIndexOf('-')) else platform}"
 
-    props["entry_point"] = "$group.${name.lowercase()}.${platform.lowercase().replace('-', '_')}.$mainClassname"
+    props["entry_point"] = "$rootPackage.$mainClassname"
+    props["platform"] = platform
+    props["root_package"] = rootPackage
 
     return props
 }
