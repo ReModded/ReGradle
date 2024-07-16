9eb3c42f7ac7a4fadab0a4530125c9b6c85b401f
Twarug <patryk280502@gmail.com>
2024-07-17 01:59:58 +0200

    Add maven plugin extension

---
+++ src/main/kotlin/dev/remodded/regradle/PluginsMaven.kt
@@ -0,0 +1,28 @@
+package dev.remodded.regradle
+
+import org.gradle.api.Project
+import java.io.File
+
+
+/**
+ * Get a plugin from the maven repository.
+ *
+ * @param pluginArtifact The plugin artifact to get.
+ * @return The plugin file.
+ */
+fun Project.getPluginFromMaven(pluginArtifact: String): File {
+    val deps = project.dependencies.create(pluginArtifact)
+
+    // Resolve the configuration to get the files
+    val tmpConfiguration = configurations.getByName("shadowRuntimeElements").copy().apply {
+        dependencies.add(deps)
+    }
+
+    val resolvedConfiguration = tmpConfiguration.resolvedConfiguration
+    val firstLvlModules = resolvedConfiguration.firstLevelModuleDependencies
+    val firstLvlModule = firstLvlModules.first()
+    val moduleArtifacts = firstLvlModule.moduleArtifacts
+    val artifact = moduleArtifacts.first()
+
+    return artifact.file
+}
